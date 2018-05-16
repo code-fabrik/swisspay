8ce8f16e4cab1c8ffae30a37a7c52e5ea01fcd4a
Lukas_Skywalker <lukas.diener@hotmail.com>
2018-05-16 16:51:41 +0200

    WIP

--- app/controllers/swisspay/saferpays_controller.rb
+++ app/controllers/swisspay/saferpays_controller.rb
@@ -1,14 +1,28 @@
 module Swisspay
   class SaferpaysController < ApplicationController
     before_action :load_session
+
+    def create
+
+      base_url = request.base_url
+      options = session['swisspay']
+
+      url, token = Swisspay::Saferpay.payment_url(base_url, options)
+      session['swisspay']['token'] = token
+      session['swisspay']['authorize'] = true
+      redirect_to url
+    end
     
     def success
-      token = session['swisspay']['token']
-      identifier = session['swisspay']['identifier']
-      valid = Swisspay::Saferpay.check_payment(identifier, token)
+      valid,trx_id,amount = Swisspay::Saferpay.check_payment(@identifier, @token)
       Rails.logger.error "NOT PAID" if !valid
 
-      Swisspay.configuration.payment_success.call(self, main_app, @identifier, :saferpay)
+      if session['swisspay']['authorize']
+        Swisspay.configuration.payment_authorized.call(self, main_app, @identifier, :saferpay, {transaction_id: trx_id})
+      else
+        identifier = Swisspay::Saferpay.capture(@identifier, trx_id, amount)
+        Swisspay.configuration.payment_success.call(self, main_app, identifier, :saferpay)
+      end
     end
 
     def fail
@@ -18,6 +32,7 @@ module Swisspay
     private
 
     def load_session
+      @token = session['swisspay']['token']
       @amount = session['swisspay']['amount']
       @identifier = session['swisspay']['identifier']
     end
--- lib/swisspay/saferpay.rb
+++ lib/swisspay/saferpay.rb
@@ -7,9 +7,10 @@ module Swisspay
     API_BASE = 'https://test.saferpay.com/api'
     PAYMENT_PAGE_INIT = '/Payment/v1/PaymentPage/Initialize'
     PAYMENT_PAGE_ASSERT = '/Payment/v1/PaymentPage/Assert'
+    PAYMENT_PAGE_CAPTURE = '/Payment/v1/Transaction/Capture'
 
-    def self.payment_url(base_url, identifier, amount, options)
-      response = post(API_BASE + PAYMENT_PAGE_INIT, payment_page_init_data(base_url, identifier, amount, options))
+    def self.payment_url(base_url, options)
+      response = post(API_BASE + PAYMENT_PAGE_INIT, payment_page_init_data(base_url, options))
       data = JSON.parse(response.delete("\\")[1..-2])
       token = data['Token']
       redirect_url = data['RedirectUrl']
@@ -19,27 +20,37 @@ module Swisspay
     def self.check_payment(identifier, token)
       response = post(API_BASE + PAYMENT_PAGE_ASSERT, payment_page_assert_data(identifier, token))
       data = JSON.parse(response.delete("\\")[1..-2])
-      data["Transaction"]["Status"] == 'AUTHORIZED'
+      result = data["Transaction"]["Status"] == 'AUTHORIZED'
+      trx_id = data["Transaction"]["Id"]
+      amount = data["Transaction"]["Amount"]["Value"]
+
+      [result, trx_id, amount]
+    end
+
+    def self.capture(identifier, transaction_id, amount)
+      response = post(API_BASE + PAYMENT_PAGE_CAPTURE, payment_page_capture_data(identifier, transaction_id, amount))
+      data = JSON.parse(response.delete("\\")[1..-2])
+      return data["OrderId"]
     end
 
     private
 
-    def self.payment_page_init_data(base_url, identifier, amount, options)
+    def self.payment_page_init_data(base_url, options)
       {
         "RequestHeader": {
           "SpecVersion": "1.6",
           "CustomerId": Swisspay.configuration.saferpay[:customer_id],
-          "RequestId": identifier,
+          "RequestId": options['identifier'],
           "RetryIndicator": 0
         },
         "TerminalId": Swisspay.configuration.saferpay[:terminal_id],
         "Payment": {
           "Amount": {
-            "Value": amount,
+            "Value": options['amount'],
             "CurrencyCode": "CHF"
           },
-          "OrderId": identifier,
-          "Description": options[:description]
+          "OrderId": options['identifier'],
+          "Description": options['description']
         },
         "ReturnUrls": {
           "Success": Swisspay::Engine.routes.url_helpers.success_saferpay_url(host: base_url),
@@ -57,6 +68,24 @@ module Swisspay
           "RetryIndicator": 0
         },
         "Token": token
+      }
+    end
+
+    def self.payment_page_capture_data(identifier, transaction_id, amount)
+      {
+        "RequestHeader": {
+          "SpecVersion": "1.6",
+          "CustomerId": Swisspay.configuration.saferpay[:customer_id],
+          "RequestId": identifier,
+          "RetryIndicator": 0
+        },
+        "TransactionReference": {
+          "TransactionId": transaction_id
+        },
+        "Amount": {
+          "Value": amount,
+          "CurrencyCode": 'CHF'
+        }
       }
     end
 
